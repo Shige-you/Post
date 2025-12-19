ca02361068baffc193085a7f768333fd1d52054d
Your Name <shigeyu28@gmail.com>
2025-12-19 14:13:26 +0900

    app.module修正

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,16 +9,17 @@ import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({isGlobal:true}),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      port:parseInt(process.env.DB_PORT||'5432',10),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
-
+      ssl:{ rejectUnauthorized: false },
     }),
     UserModule, PostModule, AuthModule],
   controllers: [AppController],
