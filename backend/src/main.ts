f808fcf90c028f27f447dc48f88d98f68bdf2991
Your Name <shigeyu28@gmail.com>
2025-12-21 02:29:00 +0900

    mainport修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = (process.env.PORT||3001,'0.0.0.0');
+  const port = process.env.PORT||3001;
   console.log(`listening on port ${port}`);
  
 
